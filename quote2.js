922f1ad09c5ae0a39cb7aced429219664acd588a
Andy Butler <abutler911@gmail.com>
2020-06-16 22:20:13 -0600

    Add files via upload

--- quote2.js
+++ quote2.js
@@ -1,39 +1,30 @@
-let sod = 0;
-let tax = 0;
-let sodTax = 0;
-let install = 0;
-let sodInstall = 0;
-let total = 0;
-let sodPrice = parseFloat(document.getElementById('sodPrice').value);
-let installPrice = 0.17;
-let taxRate = 0.0725;
-
-
 function update() {
-    alert(sodPrice);
-    footage = parseFloat(document.getElementById("footage").value);
+    // Get values from input boxes and assign to variables
+    var footage =       parseFloat(document.getElementById("footage").value);
+    var sodPrice =      parseFloat(document.getElementById("sodPsfPrice").value);
+    var installPrice =  parseFloat(document.getElementById("installPsfPrice").value);
+    var taxRate =       parseFloat(document.getElementById("taxRate").value);
 
-    // sod = numeral((parseFloat(footage) * parseFloat(sodPrice))).format('$0,0.00');
-    sod = numeral(footage * parseFloat(sodPrice)).format('$0,0.00');
+    var sod =           (parseFloat(footage) * parseFloat(sodPrice)).toFixed(2);
+    var tax =           (parseFloat(sod) * parseFloat(taxRate)).toFixed(2);
+    var sodTax =        (parseFloat(sod) + parseFloat(tax)).toFixed(2);
+    var install =       (parseFloat(footage) * parseFloat(installPrice)).toFixed(2); 
+    var sodInstall =    (parseFloat(sod) + parseFloat(install)).toFixed(2);
+    var totalOrder =    (parseFloat(sod) + parseFloat(install)).toFixed(2);
 
-    tax = numeral(parseFloat(sod) * parseFloat(taxRate)).format('$0,0.00');
-
-    sodTax = numeral((parseFloat(sod) + parseFloat(tax))).format('$0,0.00');
-    install = (parseFloat(footage) * parseFloat(installPrice)).toPrecision(5)
-    sodInstall = (parseFloat(sod) + parseFloat(install)).toPrecision(6)
-    total = (parseFloat(sod) + parseFloat(install) + parseFloat(tax)).toPrecision(6)
-    
-    document.getElementById('totalSod').innerHTML = sod;
-    document.getElementById('totalTax').innerHTML = tax;
-    document.getElementById('totalSodTax').innerHTML = sodTax;
+    document.getElementById('totalSod').innerHTML = "$" + sod;
+    document.getElementById('totalTax').innerHTML = "$" + tax;
+    document.getElementById('totalSodTax').innerHTML = "$" + sodTax;
     document.getElementById('totalInstall').innerHTML = "$" + install;
     document.getElementById('totalSodInstall').innerHTML = "$" + sodInstall;
-    document.getElementById('totalOrder').innerHTML = "$" + total;
+    document.getElementById('totalOrder').innerHTML = "$" + totalOrder;
+    
 }
 
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD', 
-    minimumFractionDigits: 2
-})
+function showMulch() {
+    var chkMulch = document.getElementById("chkMulch");
+    if(chkMulch.checked) {
+        document.getElementById('productLabel').innerHTML = "Mulch Price";
+    }
 
+}
