0281661849bd9d2fe0a3bdbab89c64531f1e93dd
abutler911 <abutler911@gmail.com>
2022-06-19 11:30:39 -0600

    functionality and format changes

--- quote.js
+++ quote.js
@@ -1,30 +1,61 @@
-function update() {
-    // Get values from input boxes and assign to variables
-    var footage =       parseFloat(document.getElementById("footage").value);
-    var sodPrice =      parseFloat(document.getElementById("sodPsfPrice").value);
-    var installPrice =  parseFloat(document.getElementById("installPsfPrice").value);
-    var taxRate =       parseFloat(document.getElementById("taxRate").value);
+const footage =       parseFloat(document.querySelector("#footage").value);
+const productPsfPrice =  parseFloat(document.querySelector("#productPsfPrice").value);
+const installPsfPrice =  parseFloat(document.querySelector("#installPsfPrice").value);
+const taxRate =       parseFloat(document.querySelector("#taxRate").value);
+const updateBtn = document.querySelector('#updateBtn');
 
-    var sod =           (parseFloat(footage) * parseFloat(sodPrice)).toFixed(2);
-    var tax =           (parseFloat(sod) * parseFloat(taxRate)).toFixed(2);
-    var sodTax =        (parseFloat(sod) + parseFloat(tax)).toFixed(2);
-    var install =       (parseFloat(footage) * parseFloat(installPrice)).toFixed(2); 
-    var sodInstall =    (parseFloat(sod) + parseFloat(install)).toFixed(2);
-    var totalOrder =    (parseFloat(sod) + parseFloat(install)).toFixed(2);
+const installPriceCont = document.querySelector('#install-price-cont');
 
-    document.getElementById('totalSod').innerHTML = "$" + sod;
-    document.getElementById('totalTax').innerHTML = "$" + tax;
-    document.getElementById('totalSodTax').innerHTML = "$" + sodTax;
-    document.getElementById('totalInstall').innerHTML = "$" + install;
-    document.getElementById('totalSodInstall').innerHTML = "$" + sodInstall;
-    document.getElementById('totalOrder').innerHTML = "$" + totalOrder;
+let total = footage * productPsfPrice;
+let tax = total * taxRate;
+let totalProduct = total + tax;
+
+console.log(installPriceCont);
+console.log(installChecked);
+console.log(footage, productPsfPrice, installPsfPrice, taxRate);
+
+console.log(total, tax, totalProduct);
+
+
+updateBtn.addEventListener('click', () => {
+    total = (footage * productPsfPrice).toFixed(2);
+    document.querySelector('#product-price').innerText = '$' + total;
+
+    tax = (total * taxRate).toFixed(2);
+    document.querySelector('#total-tax').innerText = '$' + tax;
+
+    totalProduct = (parseFloat(total) + parseFloat(tax)).toFixed(2);
+    document.querySelector('#total-product-with-tax').innerText = '$' + totalProduct;
     
-}
+});
 
-function showMulch() {
-    var chkMulch = document.getElementById("chkMulch");
-    if(chkMulch.checked) {
-        document.getElementById('productLabel').innerHTML = "Mulch Price";
-    }
 
-}
+
+
+
+
+
+
+
+
+
+
+// updateBtn.addEventListener('click', function update(footage, productPsfPrice, installPsfPrice, taxRate, installPrice) {
+//     console.log(footage);
+
+//     let total = parseFloat(footage) * parseFloat(productPsfPrice);
+//     let tax = parseFloat(total) * parseFloat(taxRate); 
+//     let totalProduct = parseFloat(total) + parseFloat(tax); 
+//     document.querySelector('#product-price').innerText = '$' + total;
+//     document.querySelector('#total-tax').innerText = '$' + tax;
+//     document.querySelector('#total-product-with-tax').innerText = '$' + totalProduct;
+// })
+
+
+
+
+// function showInstall() {
+//     installPrice.classList.toggle("display");
+// }
+
+
